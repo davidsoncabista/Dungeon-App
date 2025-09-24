c00eba4c819a68e84ebf204f2df23c173e8e618b
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-09-24 23:43:23 +0000

    perf: Adicionar preconnect hints para otimizar LCP
    Adiciona hints de pr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   
   return (
     <html lang="pt-br" suppressHydrationWarning>
+       <head>
+        <link rel="preconnect" href="https://adbelm.firebaseapp.com" />
+        <link rel="preconnect" href="https://apis.google.com" />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
