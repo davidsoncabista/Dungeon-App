6a1c437504c34afe210f3a969564991365bb461c
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-10-06 21:59:00 +0000

    feat: adicionado link voltar para a land no cabeçario da aplicação

--- src/app/(app)/header.tsx
+++ src/app/(app)/header.tsx
@@ -128,7 +128,7 @@ export function AppHeader({ user, currentUserData }: AppHeaderProps) {
     <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="/online-schedule"
+          href="/landing"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Image src="/logo.svg" alt="Dungeon App Logo" width={75} height={75} className="rounded-md" />
@@ -169,7 +169,7 @@ export function AppHeader({ user, currentUserData }: AppHeaderProps) {
            </SheetHeader>
           <nav className="grid gap-6 text-lg font-medium mt-4">
             <Link
-              href="/online-schedule"
+              href="/landing"
               className="flex items-center gap-2 text-lg font-semibold mb-4"
               onClick={() => setIsMobileMenuOpen(false)}
             >
